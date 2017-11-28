44b6af0561bf8a9676da9ff6898c7396d111b613
novitahasibuan <novitahahahasibuan@gmail.com>
2017-11-28 12:41:52 +0700

    Register Menu .

--- app/src/main/java/com/example/riffanalfarizie/makankuy/RegisterActivity.java
+++ app/src/main/java/com/example/riffanalfarizie/makankuy/RegisterActivity.java
@@ -5,18 +5,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
+        setContentView(R.layout.activity_register);
     }
-    public void clik(View v) {
-    Intent inte = new Intent(getApplicationContext(), LoginActivity.class);
-    startActivity(inte);
-}
+    public void MenuLogin(View v){
+        Intent login = new Intent(RegisterActivity.this,LoginActivity.class);
+        startActivity(login);
+    }
 
 }
